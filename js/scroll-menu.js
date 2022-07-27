8fb795fa59b450c6f6b942f911072224174f2da7
vicctorneves <vicctorneves88@gmail.com>
2022-07-26 21:50:38 -0300

    ✨ feat: footer + scroll-menu-js

--- js/scroll-menu.js
+++ js/scroll-menu.js
@@ -3,6 +3,21 @@ const menuLi = document.querySelectorAll('.menu a');
 menuLi.forEach(item => {
    item.addEventListener('click', scrollToIdonClick)
 });
+
+function getScrollTopByHref(element) {
+   const id = element.getAttribute('href');
+   return document.querySelector(id).offsetTop
+}
+
+function scrollToPosition(to) {
+   window.scroll({
+      top:to,
+      behavior: 'smooth'
+   })
+}
+
 function scrollToIdonClick(e) {
    e.preventDefault();
+   const to = getScrollTopByHref(e.target);
+   scrollToPosition(to);
 }
