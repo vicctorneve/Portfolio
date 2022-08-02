a83623c1bd5f9c13bc5b39f39e891df99419dbaa
vicctorneves <vicctorneves88@gmail.com>
2022-08-02 17:16:11 -0300

    🧪 test: Testanto responsividade

--- js/back-to-top.js
+++
@@ -1,7 +0,0 @@
-const backToHome = document.querySelector('.back-to-home');
-backToHome.addEventListener('click', function() {
-   window.scroll({
-      top: 0,
-      behavior: 'smooth'
-   })
-})
--- js/efeito-digitação.js
+++ js/efeito-digitação.js
@@ -2,6 +2,7 @@ const myName = document.querySelector('section.home h1#name');
 const profissao = document.querySelector('section.home p#profissao');
 
 function typing (msg, write) {
+   write.innerHTML = ''
    let text = msg;
    for (let i = 0; i < text.length; i++) {
       setTimeout(function(){
