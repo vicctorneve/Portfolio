bcc6ba815cfba91e9509e2ca173c47e52c8169fb
vicctorneves <vicctorneves88@gmail.com>
2022-11-30 08:19:05 -0300

    💄 feat: Tempo do loader

--- js/loader.js
+++ js/loader.js
@@ -1,7 +1,21 @@
 const loader = document.querySelector("#loader");
+const load = loader.querySelector('.load')
 
-window.addEventListener("load", vanish)
+window.addEventListener("load", function(){
+   this.setTimeout(()=>{
+      showBall()
+
+   },100)
+   setTimeout(()=>{
+      vanish()
+   },1000)
+})
+
 
 function vanish() {
    loader.classList.add('disppear')
+}
+
+function showBall(){
+   load.classList.remove('hide')
 }
